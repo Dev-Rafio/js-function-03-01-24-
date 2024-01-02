61469d94a88e41b2a44b58cf3c87a6e2eac94c06
dev-rafio <rafiomusic@gmail.com>
2024-01-02 23:04:10 +0600

    first commit

---
+++ 03-01-24 js function/function.js
@@ -0,0 +1,58 @@
+
+
+
+// function peramater
+
+function printName(firstName, lastName ){
+    console.log(firstName+" "+lastName);
+
+}
+
+printName("Rafio","Kabir") //function argument
+printName("kabir", "rafio")
+
+// veriable add 
+function printNam(firstName, lastName ){
+    console.log(firstName+" "+lastName);
+
+}
+var first='Rumi';
+var last='prokrite';
+
+printNam(first, last)
+
+//user input 
+
+
+function printNa(firstName, lastName){
+    console.log(firstName+' '+lastName);
+    
+}
+let firstName=prompt("Enter Your First Name")
+    let lastName=prompt("Enter Your last Name")
+printNa(firstName, lastName)
+
+
+
+
+
+
+//sum veriable
+function sum1(firstNum,lastNum){
+    console.log(firstNum + lastNum);
+}
+let firs=+1;
+let las=+2;
+sum1(firs,las)
+
+
+//sum input
+function sum(firstNum, lastNum){
+    console.log(firstNum+lastNum)
+
+}
+
+let firstNum=+prompt("Enter first number");
+let lastNum=+prompt("Enter last number");
+
+sum(firstNum,lastNum)

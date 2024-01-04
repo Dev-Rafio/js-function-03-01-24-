85022a438dfa9d7873d14942919e842e805598b9
dev-rafio <rafiomusic@gmail.com>
2024-01-04 23:34:10 +0600

    some add

--- js object&event-4-1-24/in.js
+++ js object&event-4-1-24/in.js
@@ -1,26 +1,34 @@
 
 // js object
-
 var myInfo={
-
 name:'Rafio',
 address:"dhaka",
-phoneNo:"01747784310",
+phoneNo:1747784310,
 sum:78,
-
 }
-console.log(myInfo.phoneNo,myInfo.address);
+console.log(myInfo.phoneNo);
 
 
-//function object
-function printr(name,address, phone,sum){
-this.name=name;
-this.address=address;
-this.phone;
-this.sum;
-}
-var print= new printr("rafio","dhaka","0176846");
-console.log(print.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,13 +39,12 @@ function Printlen() {
 }
 
 
-function Nm() {
-  document.getElementById("id2").innerHTML= Date();
-}
 
+
+//js Events/ Date()
 
 function ClickMe(){
-  document.getElementById("ID1").innerHTML = Date()
+  document.getElementById("id2").innerHTML = Date()
 
 }
 

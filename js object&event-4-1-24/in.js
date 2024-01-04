e73ab0c5006d73ca040dd7e616f14da042a59424
dev-rafio <rafiomusic@gmail.com>
2024-01-04 09:48:04 +0600

    some code add

--- js object&event-4-1-24/in.js
+++ js object&event-4-1-24/in.js
@@ -1,11 +1,44 @@
 
 // js object
 
-var printName={
+var myInfo={
 
+name:'Rafio',
+address:"dhaka",
+phoneNo:"01747784310",
+sum:78,
 
-friendName:"Rafio",
- harAge:"19",
-  mobileNum:"0174778....."
 }
-console.log(printName);
+console.log(myInfo.phoneNo,myInfo.address);
+
+
+//function object
+function printr(name,address, phone,sum){
+this.name=name;
+this.address=address;
+this.phone;
+this.sum;
+}
+var print= new printr("rafio","dhaka","0176846");
+console.log(print.name);
+
+
+
+//js Events
+
+function Printlen() {
+  document.write("HeLlo everyone' How are you ?")
+}
+
+
+function Nm() {
+  document.getElementById("id2").innerHTML= Date();
+}
+
+
+function ClickMe(){
+  document.getElementById("ID1").innerHTML = Date()
+
+}
+
+
